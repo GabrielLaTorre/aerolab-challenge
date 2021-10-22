c456bf89d2d108dec262898c99a1da8da7700dee
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2021-10-22 20:51:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const { PORT } = require('./config');
 const products = require('./routes/products');
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
-app.use(cors());
 
 
 app.use('/products', products);
@@ -16,6 +14,6 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen( port, () => {
+app.listen( PORT, () => {
     console.log(`Servidor funcionando en puerto ${port}`)
 });
