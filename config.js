d7723c4875ae6ef8500720a4a58c0275afd5ab27
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2021-10-22 21:42:43 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ const dotEnv = require('dotenv').config();
 module.exports = {
     PRODUCTS_URL: process.env.PRODUCTS_URL,
     CURRENCY_URL: process.env.CURRENCY_URL,
-    CATEGORIES_URL: process.env.CATEGORIES_URL
+    CATEGORIES_URL: process.env.CATEGORIES_URL,
+    PORT: process.env.PORT || 5000
 }
